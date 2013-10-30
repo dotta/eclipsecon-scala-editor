a18b41ea09063984e586b557fe4794177aa8d297
Mirco Dotta <mirco.dotta@typesafe.com>
2013-10-30 17:45:14 +0100

    Removed useless comments

--- src/de/vogella/rcp/editor/example/model/Person.scala
+++ src/de/vogella/rcp/editor/example/model/Person.scala
@@ -1,9 +1,6 @@
 package de.vogella.rcp.editor.example.model
 
-// Use @BeanProperty annotation to add plain Java bean getters to the class.
-case class Person(firstName: String, lastName: String)(val id: Int = Person.counter()) {
-//  override def toString(): String = firstName + " " + lastName
-}
+case class Person(firstName: String, lastName: String)(val id: Int = Person.counter())
 
 object Person {
   private var id = 0
