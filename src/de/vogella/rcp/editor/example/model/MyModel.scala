30a14397dbfa428eb49032ace816a13eadabe5be
Mirco Dotta <mirco.dotta@typesafe.com>
2013-10-30 17:44:11 +0100

    changed name of persons in model

--- src/de/vogella/rcp/editor/example/model/MyModel.scala
+++ src/de/vogella/rcp/editor/example/model/MyModel.scala
@@ -1,7 +1,7 @@
 package de.vogella.rcp.editor.example.model
 
 object MyModel {
-  val persons = Array(Person("Hans", "Nase")(), Person("Jim", "Knopf")())
+  val persons = Array(Person("Fredrik", "Nase")(), Person("Jerry", "Knopf")())
 
   def personById(id: Int): Person =
     persons.find(person => person.id == id).orNull
